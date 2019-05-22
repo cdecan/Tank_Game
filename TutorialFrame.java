4b9415d35e2a23cb9accda4a505da0bd5e2b7c0d
Sllama <50175082+Sllama@users.noreply.github.com>
2019-05-22 09:55:46 -0400

    Add files via upload

---
+++ TutorialFrame.java
@@ -0,0 +1,71 @@
+/** 
+ * this template can be used for a start menu
+ * for your final project
+ **/
+
+
+//Imports
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingUtilities;
+
+class TutorialFrame extends JFrame { 
+  
+  JFrame thisFrame;
+  
+  //Constructor - this runs first
+  TutorialFrame() { 
+    super("Start Screen");
+    this.thisFrame = this; //lol  
+    
+    //configure the window
+    this.setSize(400,700);    
+    this.setLocationRelativeTo(null); //start the frame in the center of the screen
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    this.setResizable (false);
+    
+    //Create a Panel for stuff
+    JPanel mainPanel = new JPanel();
+    mainPanel.setBackground(Color.GREEN);
+    
+    mainPanel.setLayout(new GridLayout(3,0));
+    
+    //Create a JButton for the centerPanel
+    JButton returnButton = new JButton("RETURN");
+    returnButton.addActionListener(new ButtonListener());
+    returnButton.setBackground(Color.WHITE);
+    
+    //Create a tutorial label
+    JLabel helpLabel = new JLabel("THIS IS HOW TO PLAY THE GAME");
+    
+    mainPanel.add(helpLabel);
+    mainPanel.add(returnButton);
+    
+    //add the main panel to the frame
+    this.add(mainPanel);
+    
+    //Start the app
+    this.setVisible(true);
+  }
+  
+  //This is an inner class that is used to detect a button press
+  class ButtonListener implements ActionListener {  //this is the required class definition
+    public void actionPerformed(ActionEvent event)  {  
+      System.out.println("returning to thing");
+        thisFrame.dispose();
+        new StartingFrame(); //create a new FunkyFrame (another file that extends JFrame)
+      
+    }
+    
+  }
+  
+  
+  
+}
