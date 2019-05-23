edd63187ccc79cac653008bfaf868b37a2861f57
Sllama <50175082+Sllama@users.noreply.github.com>
2019-05-23 10:21:16 -0400

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -24,8 +24,8 @@ class GameFrame extends JFrame {
   //class variable (non-static)
   static double x, y;
   static double dx, dy;
-  static Square blueSquare;
-  static RedSquare redSquare;
+  
+  Square square;
   boolean moveLeft;
   boolean moveRight;
   boolean moveUp;
@@ -46,14 +46,15 @@ class GameFrame extends JFrame {
     
     moveLeft = false;
     moveRight = false;
+    moveUp = false;
+    moveDown = false;
     
     //Set up the game panel (where we put our graphics)
-    blueSquare = new BlueSquare();
-    redSquare = new RedSquare(); 
+    square = new Square();
+    
     //JPanel panel = new JPanel();
-    this.add(redSquare);
-    this.add(blueSquare);
-
+    this.add(square);
+    
     
     MyKeyListener keyListener = new MyKeyListener(); 
     this.addKeyListener(keyListener);
@@ -75,6 +76,8 @@ class GameFrame extends JFrame {
   public void animate() { 
     this.x = (100);  //update coords
     this.y = (100);
+    this.dx = (200);
+    this.dy = (300);
     while(true){
       
       
@@ -87,51 +90,36 @@ class GameFrame extends JFrame {
   /** --------- INNER CLASSES ------------- **/
   
   // Inner class for the the game area - This is where all the drawing of the screen occurs
+  
   private class Square extends JPanel{
+    //, Graphics g2\
+   //      g.fillRect((int)dx, (int)dy, 50, 50);
     public void paintComponent(Graphics g) {   
       super.paintComponent(g); //required
+      //super.paintComponent(g2);
       setDoubleBuffered(true); 
-
-      g.fillRect((int)x, (int)y, 50, 50); //notice the x,y variables that we control from our animate method      
+      g.setColor(Color.RED); //There are many graphics commands that Java can use
+      g.fillRect((int)x, (int)y, 60, 60); //notice the x,y variables that we control from our animate method
+      g.fillRect((int)x,(int)y+23, 100, 12);
+      if (moveLeft == true){
+        x--;
+      } else if (moveRight == true){
+        x++;
+      }else if (moveUp){
+        y--;
+      }else if(moveDown){
+        y++;
+      }
+   //   g2.setColor(Color.BLUE); //There are many graphics commands that Java can use
+     // g2.fillRect((int)dx, (int)dy, 50, 50); //notice the x,y variables that we control from our animate method      
       
-      if (moveLeft == true){
-        x--;
-      } else if (moveRight == true){
-        x++;
-      }else if (moveUp){
-        y--;
-      }else if(moveDown){
-        y++;
-      }
+
     }
-  }
-  private class RedSquare extends Square{
-
-    public void paintComponent(Graphics g) {   
-      super.paintComponent(g); //required
-      setDoubleBuffered(true); 
-      g.setColor(Color.GREEN); //There are many graphics commands that Java can use
-      g.fillRect((int)x, (int)y, 50, 50); //notice the x,y variables that we control from our animate method      
-      if (moveLeft == true){
-        x--;
-      } else if (moveRight == true){
-        x++;
-      }else if (moveUp){
-        y--;
-      }else if(moveDown){
-        y++;
-      }
-    }
-  }
-  private class BlueSquare extends Square{
-
-    public void paintComponent(Graphics g) {   
-      super.paintComponent(g); //required
-      setDoubleBuffered(true); 
-      g.setColor(Color.BLUE); //There are many graphics commands that Java can use
-      g.fillRect((int)dx, (int)dy, 50, 50); //notice the x,y variables that we control from our animate method      
-
-      if (moveLeft2 == true){
+    public void paint(Graphics g) {
+      super.paint(g);
+    g.setColor(Color.BLUE);
+    g.fillRect((int)dx,(int)dy,50,50);
+          if (moveLeft2 == true){
         dx-=2;
       } else if (moveRight2 == true){
         dx+=2;
@@ -140,8 +128,10 @@ class GameFrame extends JFrame {
       }else if(moveDown2){
         dy+=2;
       }
-    }
   }
+    
+  }
+  
   // -----------  Inner class for the keyboard listener - this detects key presses and runs the corresponding code
   private class MyKeyListener implements KeyListener {
     
@@ -158,10 +148,10 @@ class GameFrame extends JFrame {
       }else if (KeyEvent.getKeyText(e.getKeyCode()).equals("A")) {
         moveLeft = true;
       }else if (KeyEvent.getKeyText(e.getKeyCode()).equals("W")) {
-  for(int i = 100; i>1; i--){
-moveUp = true;
-
-    }
+        for(int i = 100; i>1; i--){
+          moveUp = true;
+          
+        }
       }else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {  //If ESC is pressed
         //System.out.println("ESCAPE KEY!"); //close frame & quit
         System.exit(0);
