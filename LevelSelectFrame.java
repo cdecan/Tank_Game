839ffadbc077f7c879f8591120e44f8df9bfa4a4
Sllama <50175082+Sllama@users.noreply.github.com>
2019-05-30 11:29:44 -0400

    Add files via upload

---
+++ LevelSelectFrame.java
@@ -0,0 +1,88 @@
+/** 
+ * this template can be used for a start menu
+ * for your final project
+ **/
+
+
+//Imports
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingUtilities;
+
+class LevelSelectFrame extends JFrame { 
+  
+  JFrame thisFrame;
+  
+  //Constructor - this runs first
+  LevelSelectFrame() { 
+    super("Start Screen");
+    this.thisFrame = this; //lol  
+    
+    //configure the window
+    this.setSize(400,700);    
+    this.setLocationRelativeTo(null); //start the frame in the center of the screen
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    //this.setResizable (false);
+    
+    //Create a Panel for stuff
+    JPanel mainPanel = new JPanel();
+    mainPanel.setBackground(Color.GREEN);
+    
+    mainPanel.setLayout(new GridLayout(0,3));
+    
+    //Create a JButton for the centerPanel
+    JButton easyButton = new JButton("EASY");
+    easyButton.addActionListener(new ButtonListener());
+    easyButton.setBackground(Color.GREEN);
+    
+    //hard
+    JButton hardButton = new JButton("HARD");
+    hardButton.addActionListener(new ButtonListener());
+    hardButton.setBackground(Color.RED);
+    
+    //vs
+    JButton vsButton = new JButton("VS");
+    vsButton.addActionListener(new ButtonListener());
+    vsButton.setBackground(Color.PINK);
+    
+    //Create a tutorial label
+    
+    mainPanel.add(easyButton);
+    mainPanel.add(hardButton);
+    mainPanel.add(vsButton);
+
+    
+    //add the main panel to the frame
+    this.add(mainPanel);
+    
+    //Start the app
+    this.setVisible(true);
+  }
+  
+  //This is an inner class that is used to detect a button press
+  class ButtonListener implements ActionListener {  //this is the required class definition
+    public void actionPerformed(ActionEvent event)  {  
+      String command = event.getActionCommand();
+      if(command.equals("EASY")){
+      System.out.println("level 1");
+        thisFrame.dispose();
+        new GameFrameLevel1(); //create a new FunkyFrame (another file that extends JFrame)
+      }else if(command.equals("HARD")){
+        System.out.println("level 2");
+        thisFrame.dispose();
+        new GameFrameLevel2();
+      }
+    }
+    
+  }
+  
+  
+  
+}
