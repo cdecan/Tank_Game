46e53af32f2046418b12b442be58977fc29d9c1b
Sllama <50175082+Sllama@users.noreply.github.com>
2019-06-06 11:18:22 -0400

    Update TutorialFrame.java

--- TutorialFrame.java
+++ TutorialFrame.java
@@ -26,7 +26,7 @@ class TutorialFrame extends JFrame {
     this.thisFrame = this; //lol  
     
     //configure the window
-    this.setSize(400,700);    
+    this.setSize(1200,700);    
     this.setLocationRelativeTo(null); //start the frame in the center of the screen
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
     //this.setResizable (false);
@@ -43,7 +43,7 @@ class TutorialFrame extends JFrame {
     returnButton.setBackground(Color.WHITE);
     
     //Create a tutorial label
-    JLabel[] help = {new JLabel("CONTROLS: "),new JLabel("W,A,S,D ARE TO MOVE YOUR TANK"),new JLabel("Q IS TO SHOOT A BALL"),new JLabel("E IS TO TELEPORT"),new JLabel("THE OBJECTIVE OF THE GAME IS TO SHOOT THE BEACH BALL BEFORE TIME RUNS OUT"),new JLabel("GETTING HIT BY A TRAP WILL CAUSE YOU TO LOSE A LIFE"),new JLabel("YOUR SCORE IS DETERMINED BY THE TIME YOU TOOK AND THE HEALTH YOU HAVE REMAINING"),new JLabel("THE MORE HEALTH YOU HAVE AND THE LESS TIME YOU TAKE, THE HIGHER SCORE YOU WILL RECIEVE"),new JLabel("THERE IS ALSO A SECRET VS MODE, WHICH WE WILL LET YOU FIGURE OUT ON YOUR OWN ;)")};
+    JLabel[] help = {new JLabel("CONTROLS: "),new JLabel("W,A,S,D ARE TO MOVE YOUR TANK"),new JLabel("Q IS TO SHOOT A BALL"),new JLabel("E IS TO TELEPORT"),new JLabel("THE OBJECTIVE OF THE GAME IS TO SHOOT THE BEACH BALL BEFORE TIME RUNS OUT"),new JLabel("GETTING HIT BY A TRAP WILL CAUSE YOU TO DIE INSTANTLY"),new JLabel("YOUR SCORE IS DETERMINED BY THE TIME YOU TOOK"),new JLabel("THE LESS TIME YOU TAKE, THE HIGHER SCORE YOU WILL RECIEVE"),new JLabel("THERE IS ALSO A SURVIVAL MODE, IN WHICH THE TARGET IS TRYING TO SHOOT YOU! RUN OUT THE CLOCK... IF YOU CAN!")};
     mainPanel.add(returnButton);
     for(int i = 0; i < help.length; i++){
       mainPanel.add(help[i]);
