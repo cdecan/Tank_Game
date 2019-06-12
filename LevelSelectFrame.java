5ee1d309ed80625b48886f023cdc61754a2a84d6
DededeKirby <48700399+DededeKirby@users.noreply.github.com>
2019-06-12 11:08:16 -0400

    Update LevelSelectFrame.java

--- LevelSelectFrame.java
+++ LevelSelectFrame.java
@@ -21,8 +21,7 @@ import javax.swing.SwingUtilities;
 class LevelSelectFrame extends JFrame { 
   
   JFrame thisFrame;
-  Font smallFont = new Font("Comic Sans MS", Font.PLAIN, 14);
-  Font bigFont = new Font("Comic Sans MS", Font.PLAIN, 24);
+  Font font = new Font("Comic Sans MS", Font.PLAIN, 16);
   
   //Constructor - this runs first
   LevelSelectFrame() { 
@@ -43,19 +42,19 @@ class LevelSelectFrame extends JFrame {
     
     //Create a JButton for the centerPanel
     JButton easyButton = new JButton("EASY");
-    easyButton.setFont(bigFont);
+    easyButton.setFont(font);
     easyButton.addActionListener(new ButtonListener());
     easyButton.setBackground(Color.GREEN);
     
     //hard
     JButton hardButton = new JButton("HARD");
-    hardButton.setFont(bigFont);
+    hardButton.setFont(font);
     hardButton.addActionListener(new ButtonListener());
     hardButton.setBackground(Color.RED);
     
     //vs
     JButton vsButton = new JButton("SURVIVAL");
-    vsButton.setFont(bigFont);
+    vsButton.setFont(font);
     vsButton.addActionListener(new ButtonListener());
     vsButton.setBackground(Color.PINK);
     
